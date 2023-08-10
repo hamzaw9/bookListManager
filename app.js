1d239e50c831d5da40d228e041845b556b100196
Hamza Baig <mail.hamzabaig@gmail.com>
2023-08-10 23:34:10 +0500

    Add book adding functionality

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-document.addEventListener("DOMContentLoaded", () => {});
+document.addEventListener("DOMContentLoaded", () => {
+  const booksCollection = [];
+  const bookTitle = document.querySelector("#book-title");
+  const authorName = document.querySelector("#author-name");
+  const addBookBtn = document.querySelector("#add-book");
+
+  function addBook() {
+    const bookTitleValue = bookTitle.value;
+    const authorNameValue = authorName.value;
+    booksCollection.push({ title: bookTitleValue, author: authorNameValue });
+    bookTitle.value = "";
+    authorName.value = "";
+  }
+
+  addBookBtn.addEventListener("click", addBook);
+});
