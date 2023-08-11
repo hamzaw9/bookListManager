a1ae2df6ddc9fb9a96718b1e9681004bc6c561a2
Hamza Baig <mail.hamzabaig@gmail.com>
2023-08-11 20:36:23 +0500

    Add display books functionality

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
   const booksCollection = JSON.parse(localStorage.getItem("books")) || [];
+
   const bookTitle = document.querySelector("#book-title");
   const authorName = document.querySelector("#author-name");
   const addBookBtn = document.querySelector("#add-book");
@@ -16,6 +17,17 @@ document.addEventListener("DOMContentLoaded", () => {
     bookTitle.value = "";
     authorName.value = "";
   }
+
+  function displayBooks(booksCollection) {
+    const bookList = document.querySelector("#book-list");
+    booksCollection.forEach((book) => {
+      bookList.innerHTML += `<div class="book"><p>${book["title"]} by ${book["author"]}</p>
+      <button id="remove-book" type="button">Remove</button></div>`;
+      console.log(`${book["title"]} by ${book["author"]}`);
+    });
+  }
+
+  displayBooks(booksCollection);
 
   addBookBtn.addEventListener("click", addBook);
 });
