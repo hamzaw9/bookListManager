a29d00c709e45e1f93a64e5630d0cd32529950c5
Hamza Baig <mail.hamzabaig@gmail.com>
2023-08-15 15:12:11 +0500

    Refactor code to use Classes

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const booksCollection = JSON.parse(localStorage.getItem('books')) || [];
-  const bookList = document.querySelector('#book-list');
-  const bookTitle = document.querySelector('#book-title');
-  const authorName = document.querySelector('#author-name');
-  const addBookBtn = document.querySelector('#add-book');
+/* document.addEventListener("DOMContentLoaded", () => {
+  const booksCollection = JSON.parse(localStorage.getItem("books")) || [];
+  const bookList = document.querySelector("#book-list");
+  const bookTitle = document.querySelector("#book-title");
+  const authorName = document.querySelector("#author-name");
+  const addBookBtn = document.querySelector("#add-book");
 
   function updateLocalStorage() {
-    localStorage.setItem('books', JSON.stringify(booksCollection));
-    bookTitle.value = '';
-    authorName.value = '';
+    localStorage.setItem("books", JSON.stringify(booksCollection));
+    bookTitle.value = "";
+    authorName.value = "";
   }
 
   function displayBooks(booksCollection) {
-    bookList.innerHTML = '';
+    bookList.innerHTML = "";
     booksCollection.forEach((book) => {
       bookList.innerHTML += `<div class="book"><p>${book.title} by ${book.author}</p>
       <button class="remove-book" type="button">Remove</button></div>`;
@@ -23,7 +23,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const bookTitleValue = bookTitle.value;
     const authorNameValue = authorName.value;
 
-    if (bookTitleValue === '' || authorNameValue === '') {
+    if (bookTitleValue === "" || authorNameValue === "") {
       return false;
     }
 
@@ -34,9 +34,9 @@ document.addEventListener('DOMContentLoaded', () => {
   }
 
   function removeBook() {
-    const removeBookBtns = document.querySelectorAll('.remove-book');
+    const removeBookBtns = document.querySelectorAll(".remove-book");
     for (let i = 0; i < removeBookBtns.length; i += 1) {
-      removeBookBtns[i].addEventListener('click', () => {
+      removeBookBtns[i].addEventListener("click", () => {
         booksCollection.splice(i, 1);
         bookList.removeChild(bookList.children[i]);
         updateLocalStorage();
@@ -45,7 +45,62 @@ document.addEventListener('DOMContentLoaded', () => {
     }
   }
 
-  addBookBtn.addEventListener('click', addBook);
-  window.addEventListener('load', displayBooks(booksCollection));
-  bookList.addEventListener('click', removeBook);
+  addBookBtn.addEventListener("click", addBook);
+  window.addEventListener("load", displayBooks(booksCollection));
+  bookList.addEventListener("click", removeBook);
+}); */
+
+class BookManager {
+  constructor() {
+    this.booksCollection = JSON.parse(localStorage.getItem('Books')) || [];
+  }
+
+  updateLocalStorage() {
+    localStorage.setItem('Books', JSON.stringify(this.booksCollection));
+    document.querySelector('#book-title').value = '';
+    document.querySelector('#author-name').value = '';
+  }
+
+  addBook() {
+    const bookTitleValue = document.querySelector('#book-title').value;
+    const authorNameValue = document.querySelector('#author-name').value;
+
+    if (bookTitleValue && authorNameValue) {
+      const newBook = { title: bookTitleValue, author: authorNameValue };
+      this.booksCollection.push(newBook);
+      this.updateLocalStorage();
+      this.displayBooks();
+    }
+  }
+
+  displayBooks() {
+    const bookList = document.querySelector('#book-list');
+    bookList.innerHTML = '';
+    this.booksCollection.forEach((book, index) => {
+      bookList.innerHTML += `<div id=${index} class="book"><p>${book.title} by ${book.author}</p>
+      <button class="remove-book" type="button">Remove</button></div>`;
+    });
+  }
+
+  removeBook(id) {
+    this.booksCollection.splice(id, 1);
+    this.updateLocalStorage();
+    this.displayBooks();
+  }
+}
+
+const bookManager1 = new BookManager();
+
+window.addEventListener('load', () => {
+  bookManager1.displayBooks();
+});
+
+const addBookBtn = document.querySelector('#add-book');
+addBookBtn.addEventListener('click', () => {
+  bookManager1.addBook();
+});
+
+const bookList = document.querySelector('#book-list');
+bookList.addEventListener('click', (event) => {
+  bookManager1.removeBook(event.target.parentNode.id);
 });
